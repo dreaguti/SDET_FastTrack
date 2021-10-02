fe73652d003baddb4e6c7a4e59215ea4c285a425
Andrea Gutierrez <drea.guti@gmail.com>
2021-10-02 13:26:28 -0400

    Day 5

---
+++ src/main/java/Day5/ConstructorPractice.java
@@ -0,0 +1,14 @@
+package Day5;
+
+public class ConstructorPractice {
+        // constructor: a special method that has the same name as class name
+        // and there is no return type
+
+        // the purpose of using the private constructor is to stop creating an instance from that class
+        // and this class will be used for static members, like Math class
+
+        private ConstructorPractice() {
+
+        }
+
+}
---
+++ src/main/java/Day5/FinalKeyword.java
@@ -0,0 +1,15 @@
+package Day5;
+
+public class FinalKeyword {
+    // we can use final in three places
+    //	final classes >> cannot be inherited
+    //	final method >> cannot be overridden
+    // 	final variable >> cannot be changed
+
+    public static void main(String[] args) {
+        // we can create a call in anywhere for specific purposes
+        class WeirdClass{
+
+        }
+    }
+}
